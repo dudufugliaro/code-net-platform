156df7ae3b451dc02376a72c82b5ae8e43482db6
Camila Torres <camilatorresandrade@hotmail.com>
2026-06-09 14:49:59 -0300

    Criação de Mensagens
    Criação da parte de adicionar mensagens/posts no site

--- backend-django/home/models.py
+++ backend-django/home/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 
 class Mensagem(models.Model):
     titulo = models.CharField(max_length=120)
     conteudo = models.TextField()
+    autor = models.CharField(max_length=80, default="Anônimo")
     criada_em = models.DateTimeField(auto_now_add=True)
 
     class Meta:
